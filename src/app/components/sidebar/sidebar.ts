a056b4105099c92551c66204867de962c5c758ed
Shah Zaib <shahzaibshazada123@gmail.com>
2025-08-17 08:52:55 +0500

    added

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -1,11 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css'
 })
 export class Sidebar {
+  sidebar : boolean = false
+  showButton : boolean = false
+  show(){
+    this.sidebar=true
+  }
+  hide(){
+    this.sidebar=false
+  }
 
 }
+  // const btn = document.getElementById('menu') as HTMLElement 
+ 
+  // const sidebar = document.getElementById("sidebar")  as HTMLElement
+
+  // btn?.addEventListener("click", ()=> {
+  //   sidebar.style.display = "flex"
+  // })
